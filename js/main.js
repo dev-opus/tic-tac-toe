f69f46030747ffc3b678e41a1a164aa008d7b5f5
victor orlunda <vicorlunda@gmail.com>
2020-07-13 12:04:16 +0100

    create the first phase of the restart button functionality

--- js/main.js
+++ js/main.js
@@ -57,4 +57,43 @@ start.addEventListener('click', () => {
 
         return
     }
+
+    if (start.innerText === 'restart') {
+        console.log('restart baby!!!')
+        let decider = prompt('Do you really want to restart the game at this point (yes/no)?', 'no');
+
+        if (decider.toLowerCase() === 'no') return (console.log('good boy :)'));
+
+        if (decider.toLowerCase() === 'yes') {
+            const board = document.querySelector('.gameboard-container')
+
+            const holder = document.querySelector('.player-section')
+            holder.removeChild(holder.firstElementChild)
+            holder.removeChild(holder.lastElementChild)
+
+            const inputElem1 = document.createElement('div')
+            const inputElem2 = document.createElement('div')
+
+            inputElem1.classList.toggle('player')
+            inputElem2.classList.toggle('player')
+
+            inputElem1.innerHTML = '<p>Player 1</p> <input type="text" id="player1">'
+            inputElem2.innerHTML = '<p>Player 2</p> <input type="text" id="player2">'
+
+            const referenceElem = document.querySelector('.versus')
+            referenceElem.insertAdjacentElement("beforebegin", inputElem1)
+            referenceElem.insertAdjacentElement("afterend", inputElem2)
+
+            /* 
+                INSIDE THIS CHAMBER IS WHERE I GET TO:
+
+                    1 - DELETE ALL THE ITEMS IN THE GAMEBOARD ARRAY 
+                    2 - DELETE ALL THE CONTENTS ON THE CELLS OF THE GAMEBOARD
+                    3 - RESET THE PLAYERs SCORE
+            */
+
+            board.classList.toggle('hidden')
+            start.innerText = 'start'
+        }
+    }
 });
