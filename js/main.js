190f9b47d125724a73847ea05a7adad0c2ee271b
victor orlunda <vicorlunda@gmail.com>
2020-07-13 12:02:34 +0100

    create the start button functionality

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ const PlayerObjectFactory = (name, marker) => {
     const getScore = () => score;
 
     const player = document.createElement('div');
-    player.setAttribute('class', 'player');
+    player.setAttribute('class','player');
     player.innerHTML = `<p>${getName()} <span>(${getMarker()})</span></p><span class='score'>${getScore()}</span>`;
 
     return {
@@ -23,3 +23,38 @@ const PlayerObjectFactory = (name, marker) => {
         updateScore
     }
 }
+
+const start = document.querySelector('.start-restart');
+
+start.addEventListener('click', () => {
+
+    if (start.innerText === 'start') {
+
+        const name1= document.getElementById('player1').value.toUpperCase();
+        const name2 = document.getElementById('player2').value.toUpperCase();
+
+        if (!name1 || !name2) {
+            alert('Player list is incomplete!!!')
+            return
+        }
+
+        start.innerText = 'restart'
+
+        const player1 = PlayerObjectFactory(name1, 'X')
+        const player2 = PlayerObjectFactory(name2, 'O')
+
+        const holder = document.querySelector('.player-section')
+        const referenceElem = document.querySelector('.versus')
+
+        holder.removeChild(holder.firstElementChild)
+        holder.removeChild(holder.lastElementChild)
+
+        referenceElem.insertAdjacentElement("beforebegin", player1.player)
+        referenceElem.insertAdjacentElement("afterend", player2.player)
+
+        const board = document.querySelector('.gameboard-container')
+        board.classList.toggle('hidden')
+
+        return
+    }
+});
